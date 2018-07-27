684b83c724efedf89d47fa84076fa5dca49668e7
cnwobi <c.nwobi@outlook.com>
2018-07-27 18:13:27 +1000

    all category converters are implemented

---
+++ src/main/java/com/chukanwobi/recipeapp/converters/categoryConverter/CategoryCommandToCategory.java
@@ -0,0 +1,24 @@
+package com.chukanwobi.recipeapp.converters.categoryConverter;
+
+import com.chukanwobi.recipeapp.commands.CategoryCommand;
+import com.chukanwobi.recipeapp.domain.Category;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand,Category> {
+
+    @Override
+    public Category convert(CategoryCommand categoryCommand) {
+        if(categoryCommand == null){
+            return null;
+        }
+
+        Category category =  new Category();
+         category.setId(categoryCommand.getId());
+         category.setDescription(categoryCommand.getDescription());
+         category.setRecipes(categoryCommand.getRecipes());
+
+        return category;
+    }
+}
---
+++ src/test/java/com/chukanwobi/recipeapp/converters/categoryConverter/CategoryCommandToCategoryTest.java
@@ -0,0 +1,43 @@
+package com.chukanwobi.recipeapp.converters.categoryConverter;
+
+import com.chukanwobi.recipeapp.commands.CategoryCommand;
+import com.chukanwobi.recipeapp.domain.Category;
+import com.chukanwobi.recipeapp.domain.Recipe;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class CategoryCommandToCategoryTest {
+    CategoryCommandToCategory converter;
+    public static final Long LONG_VALUE = new Long(3l);
+    public static final String DESCRIPTIONS = "description";
+    public static final Set<Recipe> RECIPES= new HashSet<>();
+    @Before
+    public void setUp() throws Exception {
+        converter =  new CategoryCommandToCategory();
+    }
+
+    @Test
+    public void convert() {
+        assertNull(converter.convert(null));
+    }
+    @Test
+    public void testObjectIsReturnedWhenNotNullIsPassed(){
+        assertNotNull(converter.convert(new CategoryCommand()));
+    }
+@Test
+    public void testConvert(){
+        CategoryCommand categoryCommand= new CategoryCommand();
+        categoryCommand.setId(LONG_VALUE);
+        categoryCommand.setDescription(DESCRIPTIONS);
+        categoryCommand.setRecipes(RECIPES);
+    Category category =  converter.convert(categoryCommand);
+    assertEquals(LONG_VALUE,category.getId());
+    assertEquals(DESCRIPTIONS,category.getDescription());
+    assertEquals(RECIPES,category.getRecipes());
+}
+}

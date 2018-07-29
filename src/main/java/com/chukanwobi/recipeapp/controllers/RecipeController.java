f43e517dadafdc9cc3e2677447771ce6da2f3931
cnwobi <c.nwobi@outlook.com>
2018-07-29 22:35:18 +1000

    Can save some attributes

--- src/main/java/com/chukanwobi/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/chukanwobi/recipeapp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.chukanwobi.recipeapp.controllers;
 
+import com.chukanwobi.recipeapp.commands.RecipeCommand;
 import com.chukanwobi.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecipeController {
@@ -14,11 +17,24 @@ public class RecipeController {
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
-    @GetMapping("/recipe/show/{id}")
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe",recipeService.findById(new Long(id)));
         return "recipe/show";
 
 
+    }
+    @GetMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping( "recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand =  recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/" + savedCommand.getId()+"/show";
     }
 }

841dbfd441e52dc0300385d75ef749372c05d2eb
cnwobi <c.nwobi@outlook.com>
2018-08-15 22:46:24 +1000

    Saving image to database

--- src/main/java/com/chukanwobi/recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/chukanwobi/recipeapp/services/ImageServiceImpl.java
@@ -1,14 +1,50 @@
 package com.chukanwobi.recipeapp.services;
 
+import com.chukanwobi.recipeapp.domain.Recipe;
+import com.chukanwobi.recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Optional;
+
 @Service
 @Slf4j
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Recieved a file");
+        log.debug("Inside save image");
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+        Recipe recipe = null;
+        if (recipeOptional.isPresent()){
+            recipe = recipeOptional.get();
+        }
+        else {
+            new RuntimeException("Recipe with ID " +recipeId+ " not found");
+        }
+
+
+        try {
+
+            Byte[] imageObjectByte = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                imageObjectByte[i++] = b;
+
+            }
+            recipe.setImage(imageObjectByte);
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("IO Exception Occured", e);
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/chukanwobi/recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/com/chukanwobi/recipeapp/controllers/ImageControllerTest.java
@@ -1,15 +1,22 @@
 package com.chukanwobi.recipeapp.controllers;
 
 import com.chukanwobi.recipeapp.commands.RecipeCommand;
+import com.chukanwobi.recipeapp.domain.Recipe;
+import com.chukanwobi.recipeapp.repositories.RecipeRepository;
 import com.chukanwobi.recipeapp.services.ImageService;
+import com.chukanwobi.recipeapp.services.ImageServiceImpl;
 import com.chukanwobi.recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -22,38 +29,68 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ImageControllerTest {
-    @Mock
+
     ImageService imageService;
     @Mock
     RecipeService recipeService;
+    @Mock
+    RecipeRepository recipeRepository;
+
     ImageController controller;
     MockMvc mockMvc;
+    MockMultipartFile multipartFile;
+
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
- controller = new ImageController(imageService,recipeService);
- mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-
+        controller = new ImageController(imageService, recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        multipartFile = new MockMultipartFile("imagefile", "testing.txt",
+                "text/plain", "Chuka Nwobi com".getBytes());
+        imageService = new ImageServiceImpl(recipeRepository);
     }
+
     @Test
-    public void testUpdateForm() throws Exception{
+    public void testUpdateForm() throws Exception {
         RecipeCommand recipeCommand = new RecipeCommand();
         recipeCommand.setId(1l);
         when(recipeService.findCommandById(1l)).thenReturn(recipeCommand);
 
         mockMvc.perform(get("/recipe/1/image/update"))
                 .andExpect(status().isOk())
-        .andExpect(model().attributeExists("recipe","recipezId"))
-        .andExpect(view().name("recipe/image/form"));
+                .andExpect(model().attributeExists("recipe", "recipezId"))
+                .andExpect(view().name("recipe/image/form"));
     }
+
     @Test
-    public void testReceiveImagePost() throws Exception{
-        MockMultipartFile multipartFile = new MockMultipartFile("imagefile","testing.txt",
-                "text/plain","Chuka Nwobi com".getBytes());
+    public void testReceiveImagePost() throws Exception {
+        MockMultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt",
+                "text/plain", "Chuka Nwobi com".getBytes());
         mockMvc.perform(multipart("/recipe/1/image").file(multipartFile))
                 .andExpect(status().is3xxRedirection())
-        .andExpect(header().string("Location","/recipe/1/update"));
-verify(imageService,times(1)).saveImageFile(anyLong(),any());
+                .andExpect(header().string("Location", "/recipe/1/update"));
+        verify(imageService, times(1)).saveImageFile(anyLong(), any());
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> recipeArgumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+        //when
+        imageService.saveImageFile(1l, multipartFile);
+
+        //then
+        verify(recipeRepository, times(1)).save(recipeArgumentCaptor.capture());
+
+        Recipe savedRecipe = recipeArgumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+
+
     }
 }
